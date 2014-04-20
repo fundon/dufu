a68302b36ecd4df8b97d52cc634ef4b0b2d7c094
fundon <cfddream@gmail.com>
2014-04-21 07:10:33 +0800

    up files

---
+++ file.go
@@ -0,0 +1,55 @@
+package space
+
+import (
+  "os"
+  "bytes"
+  "io/ioutil"
+  "path/filepath"
+)
+
+type File struct {
+  Name      string
+  Path      string
+  Buffer    *bytes.Buffer
+  realpath  string
+  status    int
+}
+
+func (f *File) Written() bool {
+  return f.status != 0
+}
+
+func (f *File) Write() (err error) {
+  path, _ := filepath.Split(f.Path)
+  ospath := filepath.FromSlash(path)
+
+  if ospath != "" {
+    err = os.MkdirAll(ospath, 0777) // rwx, rw, r
+    if err != nil {
+      panic(err)
+    }
+  }
+
+  file, err := os.Create(f.Path)
+  if err != nil {
+    return
+  }
+  defer file.Close()
+
+  _, err = f.Buffer.WriteTo(file)
+  if err != nil {
+    f.status = 200
+  }
+  return err
+}
+
+func (f *File) Read() (err error) {
+  data, err := ioutil.ReadFile(f.realpath)
+  if err != nil {
+    return err
+  }
+
+  _, err = f.Buffer.ReadFrom(bytes.NewBuffer(data))
+  return err
+}
+
---
+++ return_handler.go
@@ -0,0 +1,13 @@
+package space
+
+import (
+  "reflect"
+
+  W "github.com/futurespaceio/ware"
+)
+
+func defaultReturnHandler() W.ReturnHandler {
+  return func(ctx W.Context, vals []reflect.Value) {
+    // TODO
+  }
+}
