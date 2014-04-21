6fb70db0d27fcbc989d1a17600e69df041d9327e
fundon <cfddream@gmail.com>
2014-04-22 04:58:19 +0800

    update sample

--- README.md
+++ README.md
@@ -10,14 +10,29 @@ A fast, pluggable static site generator using [ware][] in Golang.
 package main
 
 import (
+	"log"
+	"runtime"
+	"time"
+
 	"github.com/futurespaceio/space/core"
 	"github.com/futurespaceio/space/plugins/markdown"
+	mw "github.com/futurespaceio/ware"
 )
 
 func main() {
 	s := space.Classic()
+	s.Use(func(c mw.Context, fs space.Filesystem, log *log.Logger) {
+		c.Next()
+		log.Printf("Compiled %v files\n", len(fs.Files()))
+	})
 	// File Processor Middleware
 	p := s.Processor
+	p.Use(func(c mw.Context, log *log.Logger, f *space.File) {
+		start := time.Now()
+		log.Printf("File Started %s", f.Name)
+		c.Next()
+		log.Printf("File Rendered %v \n", time.Since(start))
+	})
 	p.Use(markdown.Render())
 	s.Run()
 }
--- space.go
+++ space.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	//"os"
-
+	"log"
 	"runtime"
+	"time"
 
 	"github.com/futurespaceio/space/core"
 	"github.com/futurespaceio/space/plugins/markdown"
-	//mw "github.com/futurespaceio/ware"
+	mw "github.com/futurespaceio/ware"
 )
 
 const APP_VER = "0.0.0"
@@ -18,8 +18,18 @@ func init() {
 
 func main() {
 	s := space.Classic()
+	s.Use(func(c mw.Context, fs space.Filesystem, log *log.Logger) {
+		c.Next()
+		log.Printf("Compiled %v files\n", len(fs.Files()))
+	})
 	// File Processor Middleware
 	p := s.Processor
+	p.Use(func(c mw.Context, f *space.File, log *log.Logger) {
+		start := time.Now()
+		log.Printf("File Started %s", f.Name)
+		c.Next()
+		log.Printf("File Rendered %v \n", time.Since(start))
+	})
 	p.Use(markdown.Render())
 	s.Run()
 }
